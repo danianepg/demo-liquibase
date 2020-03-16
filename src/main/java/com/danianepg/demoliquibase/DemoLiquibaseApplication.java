1c0a2bcd49b786c180f95c8ed64e532772359f12
Daniane P. Gomes <danianepg@gmail.com>
2020-03-16 20:37:52 +0000

    Add files via upload

---
+++ src/main/java/com/danianepg/demoliquibase/DemoLiquibaseApplication.java
@@ -0,0 +1,23 @@
+package com.danianepg.demoliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLiquibaseApplication {
+
+  /**
+   * When running with maven plugin
+   *
+   * mvn liquibase:generateChangelog
+   * mvn liquibase:changelogSync
+   * mvn liquibase:updateSQL
+   * mvn liquibase:update
+   *
+   * @param args
+   */
+  public static void main(final String[] args) {
+    SpringApplication.run(DemoLiquibaseApplication.class, args);
+  }
+
+}
