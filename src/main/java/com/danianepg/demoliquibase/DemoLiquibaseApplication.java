dc60c29784bdb5496dfcbcde00d65f83a5153d8f
Daniane P.Gomes <danianepg@gmail.com>
2021-06-02 19:53:33 +0200

    Update liquibase configuration

--- src/main/java/com/danianepg/demoliquibase/DemoLiquibaseApplication.java
+++ src/main/java/com/danianepg/demoliquibase/DemoLiquibaseApplication.java
@@ -6,18 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoLiquibaseApplication {
 
-  /**
-   * When running with maven plugin
-   *
-   * mvn liquibase:generateChangelog
-   * mvn liquibase:changelogSync
-   * mvn liquibase:updateSQL
-   * mvn liquibase:update
-   *
-   * @param args
-   */
-  public static void main(final String[] args) {
-    SpringApplication.run(DemoLiquibaseApplication.class, args);
-  }
+	/**
+	 * When running with maven plugin
+	 *
+	 * mvn liquibase:generateChangeLog // Takes a snapshot from the database current state 
+	 * mvn liquibase:changelogSync 
+	 * mvn liquibase:updateSQL 
+	 * mvn liquibase:update
+	 *
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		SpringApplication.run(DemoLiquibaseApplication.class, args);
+	}
 
 }
